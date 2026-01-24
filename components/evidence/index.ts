2c7fa1e919517b60d4b8189ce3b6046d2c71fc3c
admindev456 <nshahh10@gmail.com>
2026-01-24 17:53:36 -0600

    Create index.ts

---
+++ components/evidence/index.ts
@@ -0,0 +1,4 @@
+export { default as DocumentCard } from './DocumentCard';
+export { default as DocumentGrid } from './DocumentGrid';
+export { default as SourceAttribution } from './SourceAttribution';
+
