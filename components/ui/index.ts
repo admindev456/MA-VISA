55daed88c694f7d3ba1051edc975b254adf1a7be
admindev456 <nshahh10@gmail.com>
2026-01-24 18:01:35 -0600

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card';
+export { default as Badge, DotBadge } from './Badge';
+export { default as Tabs, TabList, TabTrigger, TabContent } from './Tabs';
+export { default as Modal, ModalFooter } from './Modal';
+export { default as FilterToggle, ViewToggle } from './FilterToggle';
+
